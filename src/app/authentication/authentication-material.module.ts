4cc195adec30c8b8c0eda154f573545dbbc88ea2
alexandrbelous2003 <alexandrbelous2003@gmail.com>
2021-05-21 18:37:33 +0500

    update Angular

--- src/app/authentication/authentication-material.module.ts
+++ src/app/authentication/authentication-material.module.ts
@@ -1,6 +1,20 @@
 import { NgModule } from '@angular/core';
 
+import {MatButtonModule} from '@angular/material/button';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+
+
 @NgModule({
-  exports: [],
+  exports: [
+    MatButtonModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule
+  ],
 })
 export class AuthenticationMaterialModule {}
--- src/app/authentication/authentication.module.ts
+++ src/app/authentication/authentication.module.ts
@@ -3,13 +3,25 @@ import { CommonModule } from '@angular/common';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { AuthenticationRoutingModule } from './authentication-routing.module';
 import { AuthenticationMaterialModule } from './authentication-material.module';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { ResetPasswordComponent } from './reset-password/reset-password.component';
 
 @NgModule({
+
+  declarations: [
+      ForgotPasswordComponent,
+      ResetPasswordComponent,    
+      SignInComponent,
+      SignUpComponent
+  ],
   imports: [
     FlexLayoutModule,
     CommonModule,
     AuthenticationRoutingModule,
-    AuthenticationMaterialModule,
+    AuthenticationMaterialModule
+    
   ]
 })
 export class AuthenticationModule {}
